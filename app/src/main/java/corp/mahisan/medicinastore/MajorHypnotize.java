a2c4d587aed58106c6b49cf500440e0c49a6111f
Sanchit Goel <sanchit.mgoel007@gmail.com>
2015-07-14 22:12:44 +0530

    Update MajorHypnotize.java

--- app/src/main/java/corp/mahisan/medicinastore/MajorHypnotize.java
+++ app/src/main/java/corp/mahisan/medicinastore/MajorHypnotize.java
@@ -1,6 +1,6 @@
 package corp.mahisan.medicinastore;
 
-import android.app.Activity;
+import android.app.Activity
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.animation.Animation;
