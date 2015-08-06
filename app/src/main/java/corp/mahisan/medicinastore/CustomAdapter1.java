1a7ac882eaeff500ebef5081eb6fffe8b910e8c0
Sanchit Goel <sanchit.mgoel007@gmail.com>
2015-08-06 11:28:05 +0530

    Update CustomAdapter1.java

--- app/src/main/java/corp/mahisan/medicinastore/CustomAdapter1.java
+++ app/src/main/java/corp/mahisan/medicinastore/CustomAdapter1.java
@@ -1,7 +1,7 @@
 package corp.mahisan.medicinastore;
 
 import android.content.Context;
-import android.content.Intent;
+import android.content.Intent
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
