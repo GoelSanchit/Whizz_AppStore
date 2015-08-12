169ad6647b4dc31b2ced98dbd0121c31de577f41
Sanchit Goel <sanchit.mgoel007@gmail.com>
2015-08-12 17:35:11 +0530

    Update DelhiMetro.java

--- app/src/main/java/corp/mahisan/medicinastore/DelhiMetro.java
+++ app/src/main/java/corp/mahisan/medicinastore/DelhiMetro.java
@@ -1,6 +1,6 @@
 package corp.mahisan.medicinastore;
 
-import android.app.ProgressDialo;
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.NavUtils;
